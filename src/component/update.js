3ab3ca8c9e365c1fcf74a629216606c7b9991768
codeisveryeasy <prafful@airasia.com>
2023-03-11 12:06:07 +0800

    reading location in update component

--- src/component/update.js
+++ src/component/update.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
 function UpdateSong() {
 
+    let [editvideoid, updateEditVideoid] = useState("")
+    let [editvideoviews, updateEditVideoViews] = useState(0)
+    let [editvideolikes, updateEditVideoLikes] = useState(0)
     //use useLocation() hook to get information about location
     //if any parameters are received in this location, then
     //then will be available in location instance
@@ -11,11 +14,27 @@ function UpdateSong() {
     console.log("Edit this in form: ");
     console.log(location.state)
 
-    return ( 
+    useEffect(() => {
+        updateEditVideoid(location.state.editsong.videoid)
+        updateEditVideoViews(location.state.editsong.views)
+        updateEditVideoLikes(location.state.editsong.likes)
+    }, [])
+
+
+    return (
         <div>
-            <h1>Update song with id:</h1>
+            <div>
+                <h1>Update song with id:</h1>
+                <h3>Videoid: {editvideoid}</h3>
+                <h3>Views: {editvideoviews}</h3>
+                <h3>Videoid: {editvideolikes}</h3>
+            </div>
+            <div>
+                
+            </div>
+
         </div>
-     );
+    );
 }
 
 export default UpdateSong;
