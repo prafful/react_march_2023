90b2f57bd8ece4a27e9961bc4e8c44c537ead32b
codeisveryeasy <prafful@airasia.com>
2023-03-11 14:04:55 +0800

    update done

--- src/component/update.js
+++ src/component/update.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
@@ -11,7 +12,7 @@ function UpdateSong() {
     //then will be available in location instance
     let location = useLocation()
     //console.log(location)
-    console.log("Edit this in form: ");
+    //console.log("Edit this in form: ");
     console.log(location.state)
 
     useEffect(() => {
@@ -20,6 +21,47 @@ function UpdateSong() {
         updateEditVideoLikes(location.state.editsong.likes)
     }, [])
 
+    let captureText = (event)=>{
+        //console.log(event)
+        //console.log(event.target)
+        console.log(event.target.id)
+        if (event.target.id === "id_videoid") {
+            console.log("Editing videoid")
+            console.log(event.target.value)
+            updateEditVideoid(event.target.value)
+        }
+        if (event.target.id === "id_views") {
+            console.log("Editing views")
+            console.log(event.target.value)
+            updateEditVideoViews(event.target.value)
+        }
+        if (event.target.id === "id_likes") {
+            console.log("Editing likes")
+            console.log(event.target.value)
+            updateEditVideoLikes(event.target.value)
+        }
+    }
+
+
+    let updateVideo=()=>{
+        let updateVideoObject = {
+            videoid: editvideoid,
+            likes: editvideolikes,
+            views: editvideoviews
+        }
+        console.log(updateVideoObject);
+        //call update api to update video
+        let id = location.state.editsong._id
+        axios.put("https://joyous-erin-chameleon.cyclic.app/update/song/"  + id, updateVideoObject)
+            .then((response)=>{
+                console.log(response)
+            })
+            .catch(error=>{
+                console.log(error)
+            })
+            
+
+    }
 
     return (
         <div>
@@ -27,10 +69,49 @@ function UpdateSong() {
                 <h1>Update song with id:</h1>
                 <h3>Videoid: {editvideoid}</h3>
                 <h3>Views: {editvideoviews}</h3>
-                <h3>Videoid: {editvideolikes}</h3>
+                <h3>Likes: {editvideolikes}</h3>
             </div>
-            <div>
-                
+            <div style={{ fontSize: "20px" }}>
+                <fieldset>
+                    <legend>Edit Video</legend>
+                    <div style={{ margin: "4px" }}>
+                        <span style={{
+                            display: "inline-block",
+                            width: "100px"
+                        }}>
+                            Video Id:
+                        </span>
+                        <input type="text"
+                            onChange={captureText}
+                            id="id_videoid"/>
+                    </div>
+                    <div style={{ margin: "4px" }}>
+                        <span style={{
+                            display: "inline-block",
+                            width: "100px"
+                        }}>
+                            Views:
+                        </span>
+                        <input type="number"
+                            onChange={captureText}
+                            id="id_views"/>
+                    </div>
+                    <div style={{ margin: "4px" }}>
+                        <span style={{
+                            display: "inline-block",
+                            width: "100px"
+                        }}>
+                            Likes:
+                        </span>
+                        <input type="number"
+                            onChange={captureText}
+                            id="id_likes"/>
+                    </div>
+                    <div>
+                        <button onClick={updateVideo}>Update</button>
+                    </div>
+
+                </fieldset>
             </div>
 
         </div>
